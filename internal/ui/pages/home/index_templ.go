0948e588e8231ea765500d76c2e8805fe78cca9f
Steven Lee <steven.lee@kynrai.com>
2025-09-14 13:43:17 +0100

    use base dev container image

--- internal/ui/pages/home/index_templ.go
+++ internal/ui/pages/home/index_templ.go
@@ -37,7 +37,7 @@ func layout(header, body templ.Component) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div><div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div><div class=\"p-4\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
